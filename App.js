e44e20973ea884e7433c4397a55cae263bef022a
uit2712 <phamquangvi456@gmail.com>
2021-05-22 02:09:07 +0700

    add function validator

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import {
     StyleSheet,
 } from 'react-native';
 import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { Input } from 'react-native-elements';
+import { Input, CheckBox } from 'react-native-elements';
 import { useValidatorHelper } from './helpers/validator-helper';
 
 function App() {
@@ -20,22 +20,33 @@ function App() {
         isValidateOnValueChange: true,
         listValidators: [{
             type: 'minlength',
-            errorMessage: 'Please enter a value has at least 3 characters.',
-            minlength: 3
+            errorMessage: 'Please enter a value has at least __placeholder__ characters.',
+            minlength: 9,
+            errorMessagePlaceHolder: '__placeholder__',
         }, {
             type: 'maxlength',
-            errorMessage: 'Please enter a value has max characters is 5.',
-            maxlength: 5
+            errorMessage: 'Please enter a value has max characters is __placeholder__.',
+            maxlength: 10,
+            errorMessagePlaceHolder: '__placeholder__',
         }]
     });
     const email = useValidatorHelper({
         isValidateOnValueChange: true,
         listValidators: [{
-            type: 'email',
+            type: 'function',
             errorMessage: 'Please enter a valid email adress.',
+            validate: (value) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/.test(value) === true,
         }]
     });
-
+    const [isShowPassword, setIsShowPassword] = React.useState(false);
+    const password = useValidatorHelper({
+        isValidateOnValueChange: true,
+        listValidators: [{
+            type: 'function',
+            errorMessage: 'Please enter a password has at least one character and one number.',
+            validate: (value) => /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/.test(value) === true,
+        }]
+    });
 
     return (
         <View style={styles.container}>
@@ -56,6 +67,38 @@ function App() {
                 }
                 errorStyle={{ color: 'red' }}
                 {...email}
+            />
+            <Input
+                placeholder='Password'
+                label='Your Password'
+                leftIcon={
+                    <MaterialCommunityIcon
+                        name='key-variant'
+                        size={30}
+                    />
+                }
+                secureTextEntry={isShowPassword === false}
+                errorStyle={{ color: 'red' }}
+                {...password}
+            />
+            <Input
+                placeholder='Confirm Password'
+                label='Re-enter password'
+                leftIcon={
+                    <MaterialCommunityIcon
+                        name='key-variant'
+                        size={30}
+                    />
+                }
+                secureTextEntry={isShowPassword === false}
+                errorStyle={{ color: 'red' }}
+                {...password}
+            />
+            <CheckBox
+                right
+                title='Show password'
+                checked={isShowPassword}
+                onPress={() => setIsShowPassword(!isShowPassword)}
             />
         </View>
     );
--- helpers/validator-helper.ts
+++ helpers/validator-helper.ts
@@ -1,25 +1,24 @@
 import React from 'react';
 
 type ValidatorType = {
-    type: 'email';
-    errorMessage: string;
-} | {
     type: 'minlength';
     errorMessage: string;
+    errorMessagePlaceHolder?: string;
     minlength: number;
 } | {
     type: 'maxlength';
     errorMessage: string;
+    errorMessagePlaceHolder?: string;
     maxlength: number;
+} | {
+    type: 'function';
+    errorMessage: string;
+    validate: (value: string) => boolean;
 }
 
 interface IRequestValidatorHelper {
     listValidators: ValidatorType[];
     isValidateOnValueChange: boolean;
-}
-
-function isValidEmail(email: string) {
-    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/.test(email) === true;
 }
 
 export function useValidatorHelper(request: IRequestValidatorHelper) {
@@ -36,21 +35,21 @@ export function useValidatorHelper(request: IRequestValidatorHelper) {
             const validator = request.listValidators[i];
             switch(validator.type) {
                 default: break;
-                case 'email':
-                    if (isValidEmail(value) === false) {
-                        return validator.errorMessage ?? 'Email is invalid';
-                    }
-                    break;
                 case 'minlength':
                     const minlength = validator.minlength > 0 ? validator.minlength : 1;
                     if (value.length < minlength) {
-                        return validator.errorMessage ?? `Min length is ${minlength} character${minlength > 1 ? 's' : ''}`;
+                        return validator.errorMessage.replace(validator.errorMessagePlaceHolder ?? '', `${minlength}`) ?? `Min length is ${minlength} character${minlength > 1 ? 's' : ''}`;
                     }
                     break;
                 case 'maxlength':
                     const maxlength = validator.maxlength > 0 ? validator.maxlength : 1;
                     if (value.length > maxlength) {
-                        return validator.errorMessage ?? `Max length is ${maxlength} character${maxlength > 1 ? 's' : ''}`;
+                        return validator.errorMessage.replace(validator.errorMessagePlaceHolder ?? '', `${maxlength}`) ?? `Max length is ${maxlength} character${maxlength > 1 ? 's' : ''}`;
+                    }
+                    break;
+                case 'function':
+                    if (validator.validate(value) === false) {
+                        return validator.errorMessage ?? 'Please enter a valid value.';
                     }
                     break;
             }
